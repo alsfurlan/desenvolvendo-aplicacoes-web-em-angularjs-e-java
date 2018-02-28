98bd9253067a077d3b92f6b6b38381a51bf49d32
Anderson Luís Furlan <alsfurlan@gmail.com>
2018-02-28 13:08:11 -0300

    Aula 12

--- CRUD-back/src/main/java/produtos/Produto.java
+++ CRUD-back/src/main/java/produtos/Produto.java
@@ -1,10 +1,13 @@
 package produtos;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -25,6 +28,9 @@ public class Produto implements Serializable {
     @NotNull(message = "O campo preço não pode ser nulo")
     @Min(value = 0, message = "O campo preço não pode ser negativo")
     private Double preco; 
+    
+    @OneToMany(mappedBy = "produto", fetch = FetchType.EAGER)
+    private List<Movimentacao> movimentacoes;
 
     public Produto() {
     }
@@ -58,5 +64,13 @@ public class Produto implements Serializable {
     public void setPreco(Double preco) {
         this.preco = preco;
     }
-    
+
+    public List<Movimentacao> getMovimentacoes() {
+        return movimentacoes;
+    }
+
+    public void setMovimentacoes(List<Movimentacao> movimentacoes) {
+        this.movimentacoes = movimentacoes;
+    }
+ 
 }
